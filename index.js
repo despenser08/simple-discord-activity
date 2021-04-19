ff2521ca6d9e1be0e7baf2cc34cd4069e7ce8e92
despenser08 <despenser08@gmail.com>
2021-04-19 17:48:41 -0500

    Fix RPC & Add styling

--- index.js
+++ index.js
@@ -1,57 +1,85 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
+const { app, BrowserWindow, ipcMain, Menu } = require("electron");
+const path = require("path");
 const { Client } = require("discord-rpc");
 
-let RPC;
+let RPC = new Client({ transport: "ipc" });
+
+async function updateActivity(
+  clientId,
+  { title, desc, timestamp },
+  { imageName, imageDesc },
+  { smallImageName, smallImageDesc },
+  { button1Name, button1URL },
+  { button2Name, button2URL }
+) {
+  return new Promise(async (resolve) => {
+    if (RPC.application) await RPC.destroy();
+    RPC = new Client({ transport: "ipc" });
+
+    RPC.on("ready", () => {
+      let config = {
+        details: title,
+        state: desc,
+        largeImageKey: imageName,
+        largeImageText: imageDesc,
+        smallImageKey: smallImageName,
+        smallImageText: smallImageDesc,
+      };
+
+      if (timestamp) config.startTimestamp = new Date();
+
+      if (button1Name && button1URL) {
+        if (!config.buttons) config.buttons = [];
+
+        config.buttons.push({ label: button1Name, url: button1URL });
+      }
+      if (button2Name && button2URL) {
+        if (!config.buttons) config.buttons = [];
+
+        config.buttons.push({ label: button2Name, url: button2URL });
+      }
+
+      RPC.setActivity(config, process.pid)
+        .then(() => resolve({ success: true, message: "Success!" }))
+        .catch((e) => resolve({ success: false, message: e }));
+    });
+
+    await RPC.login({ clientId }).catch((e) =>
+      resolve({ success: false, message: e })
+    );
+  });
+}
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 475,
+    height: 500,
+    icon: path.join(__dirname, "assets", "Discord.png"),
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
     },
   });
 
+  Menu.setApplicationMenu(null);
+
   win.loadFile("index.html");
 
-  ipcMain.handle("change-activity", async (e, args) => {
-    return new Promise(async (resolve) => {
-      const [clientId, title, desc, timestamp, imageName, imageDesc] = args;
-
-      if (RPC) await RPC.destroy();
-
-      RPC = new Client({ transport: "ipc" });
-
-      RPC.on("ready", () => {
-        let config = {
-          details: title,
-          state: desc,
-          largeImageKey: imageName,
-          largeImageText: imageDesc,
-        };
-
-        if (timestamp) config.startTimestamp = new Date();
-
-        RPC.setActivity(config).then(() =>
-          resolve({ success: true, message: "Success!" })
-        );
-      });
-
-      RPC.login({ clientId, scopes: ["rpc", "rpc.api"] }).catch((e) =>
-        resolve({ success: false, message: e })
-      );
-    });
-  });
+  ipcMain.handle("update-activity", async (_, args) =>
+    updateActivity(args[0], args[1], args[2], args[3], args[4], args[5])
+  );
 }
 
 app.whenReady().then(createWindow);
+
+app.on("before-quit", () => {
+  if (RPC.application) RPC.destroy();
+});
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
   }
-  if (RPC) RPC.destroy();
 });
 
 app.on("activate", () => {
--- renderer.js
+++ renderer.js
@@ -4,22 +4,40 @@ document.getElementById("values").addEventListener("submit", async (e) => {
   e.preventDefault();
 
   let clientId = document.getElementById("clientid").value;
+
   let title = document.getElementById("title").value;
   let desc = document.getElementById("desc").value;
   let timestamp = document.getElementById("timestamp").checked;
+
   let imageName = document.getElementById("imagename").value;
   let imageDesc = document.getElementById("imagedesc").value;
 
-  const res = await ipcRenderer.invoke("change-activity", [
-    clientId,
-    title,
-    desc,
-    timestamp,
-    imageName,
-    imageDesc,
-  ]);
+  let smallImageName = document.getElementById("smallimagename").value;
+  let smallImageDesc = document.getElementById("smallimagedesc").value;
+
+  let button1Name = document.getElementById("button1name").value;
+  let button1URL = document.getElementById("button1url").value;
+
+  let button2Name = document.getElementById("button2name").value;
+  let button2URL = document.getElementById("button2url").value;
 
   let resultText = document.getElementById("result");
+  let submitButton = document.getElementById("submitbtn");
+
+  submitButton.disabled = true;
+  resultText.innerHTML = "Updating...";
+  resultText.style.color = "yellow";
+
+  const res = await ipcRenderer.invoke("update-activity", [
+    clientId,
+    { title, desc, timestamp },
+    { imageName, imageDesc },
+    { smallImageName, smallImageDesc },
+    { button1Name, button1URL },
+    { button2Name, button2URL },
+  ]);
+
   resultText.innerHTML = res.message;
   resultText.style.color = res.success ? "green" : "red";
+  submitButton.disabled = false;
 });
